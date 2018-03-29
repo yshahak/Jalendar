c726d37c45ae2d51dab68410db6125a4b143a1be
yaakov <yaakov@nfibond.com>
2018-03-29 08:58:14 +0300

    finishing day details first setup

--- app/src/main/java/com/thedroidboy/jalendar/activities/DayDetailsActivity.kt
+++ app/src/main/java/com/thedroidboy/jalendar/activities/DayDetailsActivity.kt
@@ -1,13 +1,18 @@
 package com.thedroidboy.jalendar.activities
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.thedroidboy.jalendar.R
+import com.thedroidboy.jalendar.databinding.ActivityDayDetailsBinding
+import com.thedroidboy.jalendar.model.DayTimes
 
 class DayDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_day_details)
+        val binding : ActivityDayDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_day_details)
+        binding.dayTimes = DayTimes.create()
     }
+
 }
--- app/src/main/java/com/thedroidboy/jalendar/model/DayTimes.kt
+++ app/src/main/java/com/thedroidboy/jalendar/model/DayTimes.kt
@@ -1,12 +1,14 @@
 package com.thedroidboy.jalendar.model
 
+import net.sourceforge.zmanim.ZmanimCalendar
+import java.util.concurrent.TimeUnit
 
 
 /**
  * Created by Yaakov Shahak
  * on 25/03/2018.
  */
-class DayTimes(val location: String,
+data class DayTimes(val location: String,
                val dayTitle: String,
                val alos: Long,
                val sunrise: Long,
@@ -18,7 +20,8 @@ class DayTimes(val location: String,
                val minchGdola: Long,
                val minchKtana: Long,
                val sunset: Long,
-               val dusk: Long) {
+               val dusk: Long,
+               val shabbat: Shabbat?) {
 
     //    private val iconId: Int = 0
 //    private val dayTitle: String? = null
@@ -33,6 +36,26 @@ class DayTimes(val location: String,
 //    private val tzetShabbat:  
 //    private val tzetShabbat72:  
 //    private val isShabbat: Boolean = false
-
-
+    companion object {
+        fun create () : DayTimes {
+            val zmanimCalendar = ZmanimCalendar()
+            val title = "title"
+            return DayTimes("Jerusalem", title
+                    , zmanimCalendar.alosHashachar.time
+                    , zmanimCalendar.sunrise.time
+                    , zmanimCalendar.sofZmanShmaMGA.time
+                    , zmanimCalendar.sofZmanShmaGRA.time
+                    , zmanimCalendar.sofZmanTfilaMGA.time
+                    , zmanimCalendar.sofZmanTfilaGRA.time
+                    , zmanimCalendar.chatzos.time
+                    , zmanimCalendar.minchaGedola.time
+                    , zmanimCalendar.minchaKetana.time
+                    , zmanimCalendar.sunset.time
+                    , zmanimCalendar.sunset.time + TimeUnit.MINUTES.toMillis(20)
+                    , null)
+        }
+    }
 }
+
+data class Shabbat (val parasha: String, val knisatShabbbat: Long, val tzetShabbat:Long, val tzetShabbat72: Long)
+
